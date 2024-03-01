9ffbcc8809fcc8907e3d769d1f531be9f3bc9128
Williams392 <williamsvalle392@gmail.com>
2024-03-01 02:19:10 -0500

    commenter pip install requeriments

--- inmuebles/build.sh
+++ inmuebles/build.sh
@@ -6,7 +6,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt # para que instale las dependencias del proyecto.
+# pip install -r requirements.txt # para que instale las dependencias del proyecto.
 
 # Convert static asset files
 python manage.py collectstatic --no-input
